3057ba26415e01d305c7a8fa8015941e7e7c9ae9
pokorra <patrycja.pokora@gmail.com>
2021-01-08 01:13:17 +0100

    after trying few packages, installing react-snowfall package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, {useState} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Snowfall from 'react-snowfall'
 import Home from './components/Home';
 import Quiz from './components/Quiz';
 import Final from './components/Final';
 import NotFound from './components/NotFound';
 
+
+
 function App() {
   const [score, setScore] = useState(0);
   const [number, setNumber] = useState(0);
   const [isFinished, setFinished] = useState(false);
+  const [snowflake, setSnowflake] = useState(50);
+
   return (
-    <Router>
-      <Switch>
-            <Route exact path ='/' component={Home} />
-            <Route path='/quiz' component={()=> <Quiz setScore={setScore} score={score} number={number} setNumber={setNumber} isFinished={isFinished} setFinished={setFinished}/>} />
-            <Route path='/final' component={()=> <Final setScore={setScore} score={score} setFinished={setFinished}/>} />
-            <Route component={NotFound} />
-      </Switch>
-  </Router> 
+  <>
+    <Snowfall color="white" snowflakeCount={snowflake}/>
+      <Router>
+        <Switch>
+              <Route exact path ='/' component={()=> <Home setSnowflake={setSnowflake} />} />
+              <Route path='/quiz' component={()=> <Quiz setScore={setScore} score={score} number={number} setNumber={setNumber} isFinished={isFinished} setFinished={setFinished} setSnowflake={setSnowflake} />} />
+              <Route path='/final' component={()=> <Final setScore={setScore} score={score} setFinished={setFinished} setSnowflake={setSnowflake} />} />
+              <Route component={()=> <NotFound setSnowflake={setSnowflake} />} />
+        </Switch>
+    </Router> 
+  </>
+
   );
 }
 
