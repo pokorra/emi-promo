65ec1a3ac9e12972ffb608cd461e6d832fa42273
pokorra <patrycja.pokora@gmail.com>
2021-01-11 10:53:42 +0100

    exchanging browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 // import useWindowWidth from './components/useWindowWidth'
 import Snowfall from 'react-snowfall'
 import Home from './components/Home';
